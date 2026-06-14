cdf3fcd15c7005c823892fd70ca887bb830200b9
user-x45 <asukasa462@gmail.com>
2026-06-14 22:35:04 +0900

    Update and rename index.ts to index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export default {
       // 3. 指定されたURLのHTMLを取得
       const res = await fetch(targetUrl, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
         }
       });
 
